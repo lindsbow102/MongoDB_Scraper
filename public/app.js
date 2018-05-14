e73f91949ae5abc96f0f4c22c30cfd11aa47b3a5
Lindsey Bowen <lindsbow102@yahoo.com>
2018-05-14 11:45:51 -0700

    modal notes now working

--- public/app.js
+++ public/app.js
@@ -1,70 +1,70 @@
+
 //Handle Scrape button
-$("#scrape").on("click", function() {
+$("#scrape").on("click", function () {
     $.ajax({
         method: "GET",
         url: "/scrape",
-    }).done(function(data) {
+    }).done(function (data) {
         console.log(data)
         window.location = "/"
     })
 });
 
 //Handle Save Article button
-$(".save").on("click", function() {
+$(".save").on("click", function () {
     var thisId = $(this).attr("data-id");
     $.ajax({
         method: "POST",
         url: "/articles/save/" + thisId
-    }).done(function(data) {
+    }).done(function (data) {
         window.location = "/"
     })
 });
 
 //Handle Delete Article button
-$(".delete").on("click", function() {
+$(".delete").on("click", function () {
     var thisId = $(this).attr("data-id");
     $.ajax({
         method: "POST",
         url: "/articles/delete/" + thisId
-    }).done(function(data) {
+    }).done(function (data) {
         window.location = "/saved"
     })
 });
 
 //Handle Save Note button
-$(".saveNote").on("click", function() {
+$(".saveNote").on("click", function () {
     var thisId = $(this).attr("data-id");
     if (!$("#noteText" + thisId).val()) {
         alert("please enter a note to save")
-    }else {
-      $.ajax({
+    } else {
+        $.ajax({
             method: "POST",
             url: "/notes/save/" + thisId,
             data: {
-              text: $("#noteText" + thisId).val()
+                text: $("#noteText" + thisId).val()
             }
-          }).done(function(data) {
-              // Log the response
-              console.log(data);
-              // Empty the notes section
-              $("#noteText" + thisId).val("");
-              $(".modalNote").modal("hide");
-              //$(".modalNote").modal("hide");
-              window.location = "/saved"
-          });
+        }).done(function (data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+            $("#noteText" + thisId).val("");
+            $("#noteModal" + thisId).modal("hide");
+        });
     }
 });
 
 //Handle Delete Note button
-$(".deleteNote").on("click", function() {
+$(".deleteNote").on("click", function () {
+    var thisId = $(this).attr("data-id");
     var noteId = $(this).attr("data-note-id");
     var articleId = $(this).attr("data-article-id");
     $.ajax({
         method: "DELETE",
         url: "/notes/delete/" + noteId + "/" + articleId
-    }).done(function(data) {
+    }).done(function (data) {
         console.log(data)
-        $(".modalNote").modal("hide");
+        // $(".modalNote").modal("hide");
         window.location = "/saved"
     })
 });
